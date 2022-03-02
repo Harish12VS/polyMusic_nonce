00a514dca7be852b990808c4292fb2d53d50450e
Arvind <arvind.k@grootan.com>
2022-03-02 22:13:08 +0530

    to setup base for the application using react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <ChakraProvider>
+    <div className="App">
+      Web3
+    </div>
+    </ChakraProvider>
+  );
+}
+
+export default App;
