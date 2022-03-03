2934ab6bf3a1ec606cc63e44603087016f835e6b
Mohamed Arif <mohamedarifcbe@gmail.com>
2022-03-03 12:47:08 +0530

    feat: base structure hero content created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import Hero from "./containers/Hero/Hero";
 
 function App() {
   return (
     <ChakraProvider>
-    <div className="App">
-      Web3
-    </div>
+      <Box
+        w="100%"
+        h="100vh"
+        bgGradient="linear-gradient(113.49deg, #984D38 -30.3%, #181E41 58.12%)"
+      >
+        {/* <div className="App">Web3</div> */}
+        <Hero></Hero>
+      </Box>
     </ChakraProvider>
   );
 }
