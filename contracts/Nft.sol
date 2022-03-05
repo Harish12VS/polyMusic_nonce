0f8ea803aa04fca394dc13504c775825cd39016b
harishv <harishthedev@gmail.com>
2022-03-05 23:07:47 +0530

    Add NFT contract

---
+++ contracts/Nft.sol
@@ -0,0 +1,27 @@
+// SPDX-License:Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    //Take marketplace address as the contractaddress while deploying the NFT
+    constructor(address marketPlaceAddress) ERC721("MetaVerse Tokens", "FETT") {
+        contractAddress = marketPlaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
+}
